ae49cb8cc59093e5e3688d6b2a6f157d5367132f
akaz200 <akashvenkat4454@gmail.com>
2025-02-03 14:04:10 +0530

    deploying fail

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
-const BASE_URL = "http://www.omdbapi.com/";
+const BASE_URL = "https://www.omdbapi.com/";
 
 export const searchMovies = async (query, page = 1, type = "", year = "") => {
   const response = await fetch(
